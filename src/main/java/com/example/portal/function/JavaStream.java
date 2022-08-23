5e541be036bd147c728b22eca8e8e95e8eaa4102
KQT3 <kristoffer@email.com>
2022-08-23 19:51:51 +0200

    Commit

--- src/main/java/com/example/portal/function/JavaStream.java
+++ src/main/java/com/example/portal/function/JavaStream.java
@@ -1,11 +1,20 @@
 package com.example.portal.function;
 
+import com.example.portal.core.Identifier;
+import com.example.portal.core.Version;
+import com.example.portal.master.Student;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JavaStream {
     public static void main(String[] args) throws URISyntaxException, IOException {
@@ -17,6 +26,21 @@ public class JavaStream {
                 .filter(s -> s.length() > 5);
         System.out.println(parallel);
 
+        var newPersonList = nameList.stream()
+                .map(name -> Student.create(name, new Random().nextInt(50)))
+                .toList();
+
+        Function<Student, String> getName = Student::name;
+        Function<String, String> reverse = s -> new StringBuilder(s).reverse().toString();
+        Function<String, String> uppercase = String::toUpperCase;
+
+        Function<Student, String> getReverseUppercaseName = getName.andThen(reverse).andThen(uppercase);
+
+        var stringStream = newPersonList.stream().map(getReverseUppercaseName)
+                .collect(Collectors.toList());
+        Collections.reverse(stringStream);
+        System.out.println(stringStream);
     }
+
 
 }
